d1276d19cfdb3caf14bd3e0f5e1c6f1d6b382ecd
Murilo Silva <murilo.alex.silva@gmail.com>
2020-11-18 21:19:31 -0300

    Inserindo o Widget Transferencia

--- modelo_transferencia/lib/views/page_transferencia.dart
+++ modelo_transferencia/lib/views/page_transferencia.dart
@@ -20,37 +20,19 @@ class PageTransferencia extends StatelessWidget {
         height: MediaQuery.of(context).size.height,
         child: Column(
           children: [
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: TextField(
-                controller: _controllerNumeroConta,
-                style: TextStyle(fontSize: 24),
-                decoration: InputDecoration(
-                    labelText: "Numero da Conta", hintText: "0000"),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: TextField(
-                controller: _controllerValor,
-                style: TextStyle(fontSize: 24),
-                decoration:
-                    InputDecoration(labelText: "Valor", hintText: "000"),
-              ),
+            Editor(
+                controlador: _controllerNumeroConta,
+                rotulo: "Numero da Conta",
+                dica: "0000"),
+            Editor(
+              controlador: _controllerValor,
+              rotulo: "Valor",
+              dica: "00.00",
+              icone: Icons.monetization_on,
             ),
             RaisedButton(
               child: Text("Confirmar"),
-              onPressed: () {
-                debugPrint("Clicou");
-                final int numeroConta = int.tryParse(
-                    _controllerNumeroConta.text); //Para converter em int
-                final double valor = double.tryParse(
-                    _controllerValor.text); //Para converter em double
-                if (numeroConta != null && valor != null) {
-                  final transferenciaCriada = Transferencia(valor, numeroConta);
-                  print("$transferenciaCriada");
-                }
-              },
+              onPressed: () => _criaTransferencia(),
             )
           ],
         ),
@@ -68,4 +50,44 @@ class Transferencia {
   @override
   String toString() =>
       'Transferencia(valor: $valor, numeroConta: $numeroConta)';
+}
+
+class Editor extends StatelessWidget {
+  final TextEditingController controlador; //var do controlador
+  final String rotulo;
+  final String dica;
+  final IconData icone; //var do icone
+
+  //Quando coloca o construtor com {} vc deixa o mesmo, opcional, igual os Widgets do flutter
+  const Editor({Key key, this.controlador, this.rotulo, this.dica, this.icone})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: TextField(
+        controller: controlador,
+        style: TextStyle(fontSize: 24),
+        decoration: InputDecoration(
+            labelText: rotulo,
+            hintText: dica,
+            icon: icone != null ? Icon(icone) : null
+            //Seria se o icone diferente de nulo, mostrar o icone, senão ficar nulo
+            ),
+      ),
+    );
+  }
+}
+
+//Seria para criar uma função
+void _criaTransferencia() {
+  final int numeroConta =
+      int.tryParse(_controllerNumeroConta.text); //Para converter em int
+  final double valor =
+      double.tryParse(_controllerValor.text); //Para converter em double
+  if (numeroConta != null && valor != null) {
+    final transferenciaCriada = Transferencia(valor, numeroConta);
+    print("$transferenciaCriada");
+  }
 }
